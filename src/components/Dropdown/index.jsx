eec705d161971b85da5c659a508372f0a7265191
Nishit <ravalnishit81@gmail.com>
2023-08-26 00:54:40 +0530

    update projects

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FaTimes } from "react-icons/fa";
 import styled from "@emotion/styled";
 import { Link as ScrollLink } from "react-scroll";
+import SocialIcon from "../SocialIcon/SocialIcon";
 
 const SiderBar = styled.div`
   background: #151418;
@@ -82,6 +83,7 @@ function Dropdown({ isOpen, toggle }) {
         >
           Contact
         </NavLink>
+      
       </NavMenu>
     </SiderBar>
   );
--- src/data/index.js
+++ src/data/index.js
@@ -1,10 +1,10 @@
-import { BL1Icon, BL2Icon, BL3Icon, BSG1Icon, BSG2Icon, Intelli1Icon, Intelli2Icon, Intelli3Icon, Intelli4Icon, Intelli5Icon, JpMorgon1Icon, JpMorgon2Icon, JpMorgon3Icon, LocalPower1Icon, LocalPower2Icon, LocalPower3Icon, LocalPower4Icon, Ml1Icon, Ml2Icon, Ml3Icon, VB1Icon, VB2Icon} from "../assests";
+import { BL1Icon, BL2Icon, BL3Icon, BSG1Icon, BSG2Icon, Intelli1Icon, Intelli2Icon, Intelli3Icon, Intelli4Icon, Intelli5Icon, JP1Icon, JP2Icon, JP3Icon, JpMorgon1Icon, JpMorgon2Icon, JpMorgon3Icon, LocalPower1Icon, LocalPower2Icon, LocalPower3Icon, LocalPower4Icon, Ml1Icon, Ml2Icon, Ml3Icon, UG1Icon, UG2Icon, UG3Icon, UG4Icon, UG5Icon, VB1Icon, VB2Icon} from "../assests";
 
 // --------------------------------------- Project List
 
 export const ProjectList = [
     {
-        img: Intelli1Icon,
+        img: Intelli4Icon,
         imgs: [Intelli1Icon, Intelli2Icon, Intelli3Icon, Intelli4Icon, Intelli5Icon],
         title: "IntelliRecruit",
         description: " Recruiting Software to Streamline Hiring, Improve Hire Quality & Manage Administrative Tasks. IntelliRecruit helps your company build a recruiting process to fit your needs.",
@@ -29,7 +29,7 @@ export const ProjectList = [
         description: "An online food ordering system. The customers use the platform to search and discover restaurants, read and write customer generated reviews and view and upload photos, order food delivery, book an order and make payments",
         tech_stack: ['NodeJS', 'NestJS', 'ReactJS', 'MySQL', 'SocketIO', 'Bootstrap'],
         github_url: "",
-        demo_url: "https://54.156.149.85/mealpackz",
+        demo_url: "https://wappnet.com/mealpackz",
     },
     {
         img: VB1Icon,
@@ -38,12 +38,12 @@ export const ProjectList = [
         description: "Vibecoin is a block chain-enabled commerce platform that makes it easier and cheaper to acquire new customers with a new crypto",
         tech_stack: ['NodeJS', 'ExpressJS', 'Sequelize', 'socket.io', 'swagger API', 'EJS template'],
         github_url: "",
-        demo_url: "https://54.156.149.85/vibecoin",
+        demo_url: "https://wappnet.com/vibecoin",
     },
     {
         img: BL1Icon,
         imgs: [BL1Icon, BL2Icon, BL3Icon],
-        title: "Bellesa",
+        title: "Bellesa | Sextoylab",
         description: "Online portfolio site and Admin panel for inventory store system",
         tech_stack: ['ReactJS', 'NextJS', 'GraphQL', 'MongoDB', 'KeystoneJS', 'Styles-components'],
         github_url: "",
@@ -52,12 +52,30 @@ export const ProjectList = [
     {
         img: BSG2Icon,
         imgs: [BSG1Icon, BSG2Icon],
-        title: "BSG",
+        title: "BSG | Callupper",
         description: "The BSG MVNO project involved the development of a dashboard for the Mobile Virtual Network Operator (MVNO). As an MVNO, BSG acts as a reseller for wireless communication services, leveraging existing network infrastructure without holding a personal licensed frequency allocation",
         tech_stack: ['ReactJS', 'Redux', 'axios', 'Bootstrap'],
         github_url: "",
         demo_url: "https://callupper.com",
-    }
+    },
+    {
+        img: UG1Icon,
+        imgs: [UG1Icon, UG2Icon, UG3Icon, UG4Icon, UG5Icon],
+        title: "Unitgrid",
+        description: "A single gateway to access your power profile by measuring live electricity generation and consumption",
+        tech_stack: ['ReactJS', 'Redux', 'Golong', 'IoT', 'Charts'],
+        github_url: "",
+        demo_url: "https://unitgrid.in/",
+    },
+    {
+        img: JP1Icon,
+        imgs: [JP1Icon, JP2Icon, JP3Icon],
+        title: "Covr Advisor | JP Morgon",
+        description: "The concept behind this project is to Provide the insurance services to the customers from the different banks. This project is mainly made for the insurance agents for work purpose.This project is digital, white-labeled platform integrate seamlessly with the financial brands to provide life, long-term care, and health solutions from top carriers to their clients in a simple way.",
+        tech_stack: ['ReactJS', 'Redux', 'MUI', 'Typescript'],
+        github_url: "",
+        demo_url: "",
+    },
 ];
 
 // --------------------------------------- Skills
